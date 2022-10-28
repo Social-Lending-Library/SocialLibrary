19a0dd7477cdb7fa0b08e7857fc0a95c2aba2fa6
youngruiz <rlod@protonmail.com>
2022-10-28 09:12:28 -0700

    Add fragments to tabs

--- app/src/main/java/com/example/sociallibrary/MainActivity.kt
+++ app/src/main/java/com/example/sociallibrary/MainActivity.kt
@@ -2,10 +2,40 @@ package com.example.sociallibrary
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fragmentManager: FragmentManager = supportFragmentManager
+
+        // define your fragments here
+        val fragment1: Fragment = FriendsFragment()
+        val fragment2: Fragment = MyLibraryFragment()
+        val fragment3: Fragment = ProfileFragment()
+        val fragment4: Fragment = SearchFragment()
+
+        val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottom_navigation)
+
+        // handle navigation selection
+        bottomNavigationView.setOnItemSelectedListener { item ->
+            lateinit var fragment: Fragment
+            when (item.itemId) {
+                R.id.action_friends -> fragment = fragment1
+                R.id.action_my_library -> fragment = fragment2
+                R.id.action_profile -> fragment = fragment3
+                R.id.action_search -> fragment = fragment4
+            }
+            fragmentManager.beginTransaction().replace(R.id.rlContainer, fragment).commit()
+            true
+        }
+
+        // Set default selection
+        bottomNavigationView.selectedItemId = R.id.action_friends
+
     }
 }
