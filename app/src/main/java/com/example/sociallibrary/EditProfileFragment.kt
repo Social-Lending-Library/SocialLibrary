3dfd7272eb331dd93618fdd7127ca0e3a52da944
steelemeg <megan.marshall.steele@gmail.com>
2022-11-09 17:42:25 -0600

    Profile editing added

--- app/src/main/java/com/example/sociallibrary/EditProfileFragment.kt
+++ app/src/main/java/com/example/sociallibrary/EditProfileFragment.kt
@@ -6,6 +6,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageButton
 import android.widget.TextView
 import com.parse.ParseException
@@ -46,16 +48,50 @@ class EditProfileFragment : Fragment() {
         val userObjectId = requireArguments().getString("userObjectId", "None")
         Log.i("profile edit", "The Profile Fragment has userObjectID " + userObjectId.toString())
 
+        val etProfileName = view.findViewById<EditText>(R.id.etProfileName)
+        val etProfileEmail = view.findViewById<EditText>(R.id.etProfileEmail)
+        val etProfileBlurb = view.findViewById<EditText>(R.id.etProfileBlurb)
+
         val getUserInfo = ParseQuery<ParseObject>("_User")
         try {
             //val userInfo = getUserInfo.find()
             val userInfoTest = getUserInfo[userObjectId]
             Log.v("profile name", userInfoTest.get("firstName").toString())
+            etProfileName.setHint(userInfoTest.get("firstName").toString())
+            etProfileEmail.setHint(userInfoTest.get("email").toString())
+            etProfileBlurb.setHint(userInfoTest.get("aboutMe").toString())
+
+            val saveButton = view.findViewById<Button>(R.id.btnProfileSave)
+            saveButton.setOnClickListener(){
+                val updatedName = etProfileName.text
+                val updatedEmail = etProfileEmail.text
+                val updatedBlurb = etProfileBlurb.text
+                userInfoTest.put("firstName", updatedName)
+                userInfoTest.put("email", updatedEmail)
+                userInfoTest.put("aboutMe", updatedBlurb)
+                userInfoTest.saveInBackground()
+
+                val fragmentManager = activity?.supportFragmentManager
+                if (fragmentManager != null) {
+                    fragmentManager.popBackStack()
+                }
+            }
         }
         catch (e: ParseException){
             Log.v("profile error" , "error querying user table: $e")
         }
 
+
+
+        val libraryButton = view.findViewById<Button>(R.id.btnChangeNowReading)
+        libraryButton.setOnClickListener(){
+            val transaction = activity?.supportFragmentManager?.beginTransaction()
+            if (transaction != null) {
+                transaction.replace(R.id.rlContainer, MyLibraryFragment.newInstance())
+                transaction.disallowAddToBackStack()
+                transaction.commit()
+            }
+        }
         // Inflate the layout for this fragment
         return view
     }
--- app/src/main/java/com/example/sociallibrary/MyLibraryFragment.kt
+++ app/src/main/java/com/example/sociallibrary/MyLibraryFragment.kt
@@ -127,11 +127,10 @@ class MyLibraryFragment : Fragment() {
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
-        fun newInstance(param1: String, param2: String) =
+        fun newInstance() =
             MyLibraryFragment().apply {
                 arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+
                 }
             }
     }
