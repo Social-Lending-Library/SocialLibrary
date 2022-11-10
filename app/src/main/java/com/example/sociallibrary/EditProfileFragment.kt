298d771639b00577411749c5c4f54e1907cb6205
steelemeg <megan.marshall.steele@gmail.com>
2022-11-09 18:17:54 -0600

    Profile editing working

--- app/src/main/java/com/example/sociallibrary/EditProfileFragment.kt
+++ app/src/main/java/com/example/sociallibrary/EditProfileFragment.kt
@@ -1,15 +1,18 @@
 package com.example.sociallibrary
 
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageButton
 import android.widget.TextView
+import androidx.core.content.ContextCompat.getSystemService
 import com.parse.ParseException
 import com.parse.ParseObject
 import com.parse.ParseQuery
@@ -56,19 +59,52 @@ class EditProfileFragment : Fragment() {
         try {
             //val userInfo = getUserInfo.find()
             val userInfoTest = getUserInfo[userObjectId]
-            Log.v("profile name", userInfoTest.get("firstName").toString())
-            etProfileName.setHint(userInfoTest.get("firstName").toString())
-            etProfileEmail.setHint(userInfoTest.get("email").toString())
-            etProfileBlurb.setHint(userInfoTest.get("aboutMe").toString())
+            Log.v("profile existing info", userInfoTest.keySet().toString())
+            val currentName = userInfoTest.get("firstName").toString()
+            val currentEmail = userInfoTest.get("email").toString()
+            val currentBlurb = userInfoTest.get("aboutMe").toString()
+            if (currentName != null && currentName != "null") { etProfileName.setText(currentName) }
+            if (currentEmail != null && currentEmail != "null") { etProfileEmail.setText(currentEmail) }
+            if (currentBlurb != null && currentBlurb != "null") { etProfileBlurb.setText(currentBlurb) }
+
+            etProfileName.setOnFocusChangeListener { _, hasFocus ->
+                if (hasFocus) {
+
+                } else {
+                    val imm = requireActivity().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                    imm.hideSoftInputFromWindow(requireView().windowToken, 0)
+                }
+            }
+
+            etProfileEmail.setOnFocusChangeListener { _, hasFocus ->
+                if (hasFocus) {
+
+                } else {
+                    val imm = requireActivity().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                    imm.hideSoftInputFromWindow(requireView().windowToken, 0)
+                }
+            }
+
+            etProfileBlurb.setOnFocusChangeListener { _, hasFocus ->
+                if (hasFocus) {
+                    Log.v("focus test", "blurb")
+                } else {
+                    val imm = requireActivity().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                    imm.hideSoftInputFromWindow(requireView().windowToken, 0)
+                    Log.v("focus test", "blurb false")
+                }
+            }
 
             val saveButton = view.findViewById<Button>(R.id.btnProfileSave)
             saveButton.setOnClickListener(){
                 val updatedName = etProfileName.text
                 val updatedEmail = etProfileEmail.text
                 val updatedBlurb = etProfileBlurb.text
-                userInfoTest.put("firstName", updatedName)
-                userInfoTest.put("email", updatedEmail)
-                userInfoTest.put("aboutMe", updatedBlurb)
+                Log.v("profile test", updatedName.toString())
+
+                userInfoTest.put("firstName", updatedName.toString())
+                userInfoTest.put("email", updatedEmail.toString())
+                userInfoTest.put("aboutMe", updatedBlurb.toString())
                 userInfoTest.saveInBackground()
 
                 val fragmentManager = activity?.supportFragmentManager
@@ -106,4 +142,14 @@ class EditProfileFragment : Fragment() {
                 }
             }
     }
+
+    private fun closeKeyBoard() {
+        val view = getActivity()?.currentFocus
+        if (view != null) {
+            val imm = requireActivity().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(requireView().windowToken, 0)
+        }
+    }
+
 }
+
